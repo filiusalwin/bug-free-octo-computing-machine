e704c5798b221361e1b8f700f41ae586196e9e07
PotatoesFall <markarkossa@gmail.com>
2020-09-08 17:30:08 +0200

    show items on bill instead of next to items

--- src/main/resources/static/js/order.js
+++ src/main/resources/static/js/order.js
@@ -1,38 +1,70 @@
+// helper for updateBill()
+function updateProductList(names, counts) {
+    var productBox = document.getElementById("billProductList");
+    productBox.innerHTML = "";
+
+    for (var i = 0; i < names.length; i++) {
+        productBox.innerHTML += counts[i] + " " + names[i] + "<br>"
+    }
+}
+
+// helper for updateBill()
 function updateTotalPrice(priceInCents) {
     const formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'EUR',
         minimumFractionDigits: 2
     })
+
+    // format number as currency
     var formatted = formatter.format(priceInCents / 100);
+
+    // put formatted string in <span id="totalPrice"> tag
     document.getElementById("totalPrice").innerHTML = formatted;
 }
 
-function calculateTotalPrice() {
+// updates the displayed Bill with items, counts and total Price
+function updateBill() {
+    // get all <li> tags in the productList
     var productList = document.getElementById("productList");
     var products = productList.getElementsByTagName("li");
 
+    // prepare totalprice, productnames and counts
     var total = 0;
+    var productNames = [];
+    var productCounts = [];
 
+    // go through each <li> tag
     for (let productLi of products) {
+        // Get the value of the <input type="hidden" /> tag in each <li>
         var countBox = productLi.getElementsByClassName("productCountBox")[0];
         var count = parseInt(countBox.value);
 
+        // get product name and price, added as attributes via thymeleaf
+        var productName = productLi.getAttribute("productName");
         var price = parseInt(productLi.getAttribute("productPrice"));
 
+        // add to total, and if more than 0, add to list of products and counts
         total += count * price;
+        if (count != 0) {
+            productCounts.push(count);
+            productNames.push(productName);
+        }
     }
 
     updateTotalPrice(total);
+    updateProductList(productNames, productCounts);
 }
 
+// increase product count by 1
 function addProduct(id) {
     var field = document.getElementById("productCount" + id);
     field.value = parseInt(field.value) + 1;
 
-    calculateTotalPrice();
+    updateBill();
 }
 
+// decrease product count by 1
 function removeProduct(id) {
     var field = document.getElementById("productCount" + id);
     var value = field.value;
@@ -40,5 +72,5 @@ function removeProduct(id) {
         field.value = parseInt(field.value) - 1;
     }
 
-    calculateTotalPrice();
+    updateBill();
 }
