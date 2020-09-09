3cf7b871badbd68ea43184f0ea58fa29a5994dfb
PotatoesFall <markarkossa@gmail.com>
2020-09-09 13:50:25 +0200

    add script for adding products to db quickly.

---
+++ src/main/resources/scripts/add_products.sql
@@ -0,0 +1,14 @@
+INSERT INTO bacchux.product (product_id,name,price)
+	VALUES ('1','Bier',150);
+INSERT INTO bacchux.product (product_id,name,price)
+	VALUES ('2','Water',70);
+INSERT INTO bacchux.product (product_id,name,price)
+	VALUES ('3','Fris',130);
+INSERT INTO bacchux.product (product_id,name,price)
+	VALUES ('4','Wijn',270);
+INSERT INTO bacchux.product (product_id,name,price)
+	VALUES ('5','Borrelnootjes',300);
+INSERT INTO bacchux.product (product_id,name,price)
+	VALUES ('6','Tosti Kaas',200);
+INSERT INTO bacchux.product (product_id,name,price)
+	VALUES ('7','Tosti Ham & Kaas',250);
