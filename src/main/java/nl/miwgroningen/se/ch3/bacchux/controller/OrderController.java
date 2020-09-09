f11d69229ff604045f633c63ac1c99d9b4c4a4f4
PotatoesFall <markarkossa@gmail.com>
2020-09-09 16:50:56 +0200

    make new page, no functionality yet

--- src/main/java/nl/miwgroningen/se/ch3/bacchux/controller/OrderController.java
+++ src/main/java/nl/miwgroningen/se/ch3/bacchux/controller/OrderController.java
@@ -1,6 +1,7 @@
 package nl.miwgroningen.se.ch3.bacchux.controller;
 
 import nl.miwgroningen.se.ch3.bacchux.repository.ProductRepository;
+import nl.miwgroningen.se.ch3.bacchux.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +13,18 @@ public class OrderController {
     @Autowired
     ProductRepository productRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/order")
-    protected String showCatalog(Model model){
+    protected String showCatalog(Model model) {
         model.addAttribute("allProducts", productRepository.findAll());
         return "order";
     }
 
-
-
+    @GetMapping("/order/new/prepaid")
+    protected String addPrepaidCustomer(Model model) {
+        model.addAttribute("allUsers", userRepository.findAll());
+        return "order_new_prepaid_customer";
+    }
 }
