5b006258d7a097c37fe964c7c7996a6bbf8c9914
filiusalwin <filiusalwin@gmail.com>
2020-09-15 14:50:47 +0200

    BAC 39 Datalist added, selection not working

---
+++ src/main/resources/static/js/userOverview.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    console.log($("#CreditAllowedHidden").val());
+    if ($("#CreditAllowedHidden").val() == 'true') {
+        $("#Credit-Choice").show();
+        $("#Credit-Choice-Label").show();
+    } else {
+        $("#Credit-Choice").hide();
+        $("#Credit-Choice-Label").hide();
+    }
+    if ($("#PrepaidAllowedHidden").val() == 'true') {
+        $("#Prepaid-Choice").show();
+        $("#Prepaid-Choice-Label").show();
+    } else {
+        $("#Prepaid-Choice").hide();
+        $("#Prepaid-Choice-Label").hide();
+    }
+    $("#Credit").click(function() {
+        $("#Credit-Choice").toggle();
+        $("#Credit-Choice-Label").toggle();
+    });
+    $("#Prepaid").click(function() {
+        $("#Prepaid-Choice").toggle();
+        $("#Prepaid-Choice-Label").toggle();
+    });
+});
+function UpdateSelectedUser(){
+    var UserId = {}
+
+}
