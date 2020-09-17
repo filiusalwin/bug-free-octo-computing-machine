b0971322d15ea32afadf815069170a2ef2cff851
ViNguyen91 <vinguyen91991@gmail.com>
2020-09-17 22:44:00 +0200

    custome logIn page with a warning of unathorized login. Login is the homepage and order is the default page.

--- src/main/java/nl/miwgroningen/se/ch3/bacchux/configuration/SecurityConfiguration.java
+++ src/main/java/nl/miwgroningen/se/ch3/bacchux/configuration/SecurityConfiguration.java
@@ -8,32 +8,52 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     BacchuxUserDetailsService bacchuxUserDetailsService;
+
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(bacchuxUserDetailsService);
     }
 
+    //encode password from user
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    // roles Barmanager allow to access /all
+    // roles Bartender only allow to access /order/**
+    // custom 403 access denied handler
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .and().formLogin();
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/user/**", "/catalog/**").hasRole("BARMANAGER")
+                .antMatchers("/order/**").hasAnyRole("BARMANAGER", "BARTENDER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
     }
 }
---
+++ src/main/java/nl/miwgroningen/se/ch3/bacchux/controller/LoginController.java
@@ -0,0 +1,18 @@
+package nl.miwgroningen.se.ch3.bacchux.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/403")
+    public String error403() {
+        return "/403";
+    }
+}
