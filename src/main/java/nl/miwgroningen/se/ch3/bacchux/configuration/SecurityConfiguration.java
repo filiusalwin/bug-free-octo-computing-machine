27cd1dc0957effa491646d9a4637c1205a4a29c3
ViNguyen91 <vinguyen91991@gmail.com>
2020-09-21 18:35:09 +0200

    BAC-83 Log in screen without nav bar

--- src/main/java/nl/miwgroningen/se/ch3/bacchux/configuration/SecurityConfiguration.java
+++ src/main/java/nl/miwgroningen/se/ch3/bacchux/configuration/SecurityConfiguration.java
@@ -43,13 +43,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-          /*      .antMatchers("/user/**", "/catalog/**").hasRole("BARMANAGER")
+                .antMatchers("/user/**", "/catalog/**").hasRole("BARMANAGER")
                 .antMatchers("/order/**").hasAnyRole("BARMANAGER", "BARTENDER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")*/
-                /*.permitAll()*/
+                .loginPage("/login")
+                .permitAll()
                 .and()
                 .logout()
                 .permitAll()
