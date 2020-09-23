6eda8783529c07dd3632eada84ea2ce57cb6096f
ViNguyen91 <vinguyen91991@gmail.com>
2020-09-23 10:44:59 +0200

    BAC-71 set up the profile function to change PIN

--- src/main/java/nl/miwgroningen/se/ch3/bacchux/controller/ProfileController.java
+++ src/main/java/nl/miwgroningen/se/ch3/bacchux/controller/ProfileController.java
@@ -6,8 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ProfileController {
 
+    @GetMapping("/profile")
+    protected String changeProfile() {
+        return "profileOverview";
+    }
+
     @GetMapping("/profile/password")
     protected String changePassword() {
         return "changePassword";
     }
+
+    @GetMapping("/profile/pin")
+    protected String changePin() {
+        return "changePin";
+    }
+
 }
