c4cd898ca0e6e1fb710876d399c0e6cdfbf8898b
filiusalwin <filiusalwin@gmail.com>
2020-10-02 15:03:22 +0200

    BAC-55 added navbar.js with time out function and reset on mouse move, mouse click, key press

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
-    var timeout = setTimeout(lockScreen,100000);
-    if(window.event) {
-        clearTimeout(timeout);
-        timeout = setTimeout(lockScreen,100000);
-    }
 
+    timeout = setTimeout(lockScreen, 10000);
+
+    $(window).click(function() {
+        console.log("click")
+        resetTimeout();
+    })
+    $(window).keyup(function() {
+        console.log("key")
+        resetTimeout();
+    })
+    $(window).mousemove(function() {
+        console.log("mouse")
+       resetTimeout(timeout);
+    })
 });
 // 5 min is 18000000 ms
+var timeout;
 
 function lockScreen() {
-    window.location("/lockout?error");
+    window.location.assign("/lockout?error");
+}
+
+function resetTimeout(){
+    clearTimeout(timeout);
+    timeout = setTimeout(lockScreen, 18000000);
 }
