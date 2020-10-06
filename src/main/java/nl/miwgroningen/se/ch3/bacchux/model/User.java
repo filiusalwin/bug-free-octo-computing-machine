6d2893f26e9ecefc0f277e50e8f262a3a0c9b697
ViNguyen91 <vinguyen91991@gmail.com>
2020-10-06 22:12:54 +0200

    BAC-96 small fixes

--- src/main/java/nl/miwgroningen/se/ch3/bacchux/model/User.java
+++ src/main/java/nl/miwgroningen/se/ch3/bacchux/model/User.java
@@ -143,6 +143,6 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User user) {
-        return username.compareTo(user.getUsername());
+        return name.compareTo(user.getName());
     }
 }
