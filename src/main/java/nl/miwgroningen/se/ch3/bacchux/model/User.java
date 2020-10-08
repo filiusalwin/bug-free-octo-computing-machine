adb3808df959a50804434120e35e3108bdb396ba
ViNguyen91 <vinguyen91991@gmail.com>
2020-10-08 10:22:06 +0200

    BAC-96 Users are sorted by name and case insensitive

--- src/main/java/nl/miwgroningen/se/ch3/bacchux/model/User.java
+++ src/main/java/nl/miwgroningen/se/ch3/bacchux/model/User.java
@@ -167,6 +167,6 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User user) {
-        return name.compareTo(user.getName());
+        return name.toLowerCase().compareTo(user.getName().toLowerCase());
     }
 }
