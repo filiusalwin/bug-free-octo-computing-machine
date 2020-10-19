9347eed2e3926b732f9364104c054055052345bd
filiusalwin <filiusalwin@gmail.com>
2020-10-19 16:33:53 +0200

    BAC-130 fixed bug, balance is now shown in euros`

--- src/main/java/nl/miwgroningen/se/ch3/bacchux/model/User.java
+++ src/main/java/nl/miwgroningen/se/ch3/bacchux/model/User.java
@@ -6,6 +6,8 @@ import java.util.List;
 @Entity
 public class User implements Comparable<User> {
 
+    private static final int CENTS_PER_EURO = 100;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer userId;
@@ -37,6 +39,16 @@ public class User implements Comparable<User> {
     private boolean prepaidAllowed = true;
 
     private Integer balance;
+
+    public String balanceEuro(){
+        double balanceInEuro;
+        if (balance == null) {
+            balanceInEuro = 0;
+        } else {
+            balanceInEuro = (double) balance / CENTS_PER_EURO;
+        }
+        return String.format("€%.2f", balanceInEuro);
+    }
 
     public Integer getCreditTotal() {
         int total = 0;
