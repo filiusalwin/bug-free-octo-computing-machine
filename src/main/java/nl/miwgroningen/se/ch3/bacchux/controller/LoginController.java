d17c1be0c7dd3b51ed23478a4a5773a4607e6883
ViNguyen91 <vinguyen91991@gmail.com>
2020-10-21 21:43:38 +0200

    BAC-109 return to the previous page before lockscreen + show the name of the user in lockscreen

--- src/main/java/nl/miwgroningen/se/ch3/bacchux/controller/LoginController.java
+++ src/main/java/nl/miwgroningen/se/ch3/bacchux/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -80,11 +81,12 @@ public class LoginController {
     }
 
     @GetMapping("/lockout")
-    public String lockout() {
+    public String lockout(Model model) {
         Optional<User> user = getCurrentUser();
         if (user.isEmpty()) {
             return "redirect:/login";
         }
+        model.addAttribute("userName", user.get().getName());
         currentSession.setLockscreenEnabled(true);
         return "lockscreen";
     }
